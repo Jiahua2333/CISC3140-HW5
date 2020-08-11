2fdf3c4b66f7c5895f488bbebac211c369bc7725
Jiahua Liao <ljh15088131916@gmail.com>
2020-08-11 19:18:41 -0400

    Updates

---
+++ precache-manifest.6b503a9ff09e7aa3634921a58a843f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22503e578c6cc19d19698c08be5b5e3a",
+    "url": "/CISC3140-HW5/index.html"
+  },
+  {
+    "revision": "578c2564b4d3284cc7d9",
+    "url": "/CISC3140-HW5/static/css/main.ad0fd08f.chunk.css"
+  },
+  {
+    "revision": "8d34ab91cc7b9420d3f2",
+    "url": "/CISC3140-HW5/static/js/2.16e43bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CISC3140-HW5/static/js/2.16e43bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578c2564b4d3284cc7d9",
+    "url": "/CISC3140-HW5/static/js/main.58a95d4f.chunk.js"
+  },
+  {
+    "revision": "7be04a1e997da5d8a5af",
+    "url": "/CISC3140-HW5/static/js/runtime-main.e5c3a527.js"
+  }
+]);
